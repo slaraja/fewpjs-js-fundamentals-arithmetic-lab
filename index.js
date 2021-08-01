ff335950d735da51ee112dfda2394f07ea3e1b0f
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-01 08:01:42 -0400

    commit tree 22367caa07e9f7193800830aaec9a9a72e5307ab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2
+let num2 = 31
+let num3 = 10
+let num4 = 6
 
-// Write your code here
+//Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
+let multiply = num1 * num2 
+
+//generates a random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1
+
+//equation that calculates a remainder that is equal to 4
+let mod = num3 % num4
+
+//return "20" as the highest number in the set
+let max = Math.max(20)
